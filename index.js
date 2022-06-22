0305dfdc4d9c0188caf187939fb728c33ef86e47
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-22 16:26:35 +0300

    connect reqs to mongodb

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ morgan.token("body", (req, res) => {
 });
 
 // Middlewares
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 app.use(cors());
-app.use(express.static("build"));
 app.use(errorHandler);
 
 // Calls
@@ -36,6 +36,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
+// GET
 app.get("/api/notes", (req, res) => {
   return Note.find({}).then((notes) => {
     res.json(notes);
@@ -47,9 +48,8 @@ app.get("/api/notes/:id", (req, res, next) => {
 
   Note.findById(id)
     .then((note) => {
-      console.log(id);
       if (note) {
-        response.json(note);
+        res.json(note);
       } else {
         console.log("no such note in db");
         response.status(404).end();
@@ -60,6 +60,7 @@ app.get("/api/notes/:id", (req, res, next) => {
     });
 });
 
+// POST
 app.post("/api/notes", (req, res) => {
   const body = req.body;
 
@@ -78,11 +79,34 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+// DELETE
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      if (!result) {
+        console.log("no such note");
+        res.status(404).end();
+      } else {
+        res.status(204).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  response.status(204).end();
+// PUT
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // PORT
