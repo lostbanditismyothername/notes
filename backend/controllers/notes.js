32de8a004d82c274bd1affa59377a9ceadcc8f79
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-27 14:51:03 +0300

    convert callback hell to async/await

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,77 +1,81 @@
 const notesRouter = require("express").Router();
+const { Error } = require("mongoose");
 const Note = require("../models/note");
 const logger = require("../utils/logger");
 
 // GET ALL
-notesRouter.get("/", (req, res) => {
-  Note.find({}).then((notes) => res.json(notes));
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({});
+  res.json(notes);
 });
 
 // GET INDIVIDUAL
-notesRouter.get("/:id", (req, res, next) => {
+notesRouter.get("/:id", async (req, res, next) => {
   const id = req.params.id;
+  const note = await Note.findById(id);
 
-  Note.findById(id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        logger.error("no such note in db");
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
+  try {
+    if (note) {
+      res.json(note);
+    } else {
+      logger.error("no such no in db");
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // POST
-notesRouter.post("/", (req, res) => {
+notesRouter.post("/", async (req, res) => {
   const body = req.body;
 
   if (!body.content) {
     return res.status(400).json({ error: "content misssing" });
   }
 
-  const note = new Note({
+  const noteObj = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
 
-  note.save().then((savedNote) => {
-    res.json(savedNote);
-  });
+  const note = await noteObj.save();
+
+  res.json(note);
 });
 
 // PUT
-notesRouter.put("/:id", (req, res, next) => {
+notesRouter.put("/:id", async (req, res, next) => {
   const body = req.body;
 
-  const note = {
+  const newNote = {
     content: body.content,
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then((updatedNote) => {
-      res.json(updatedNote);
-    })
-    .catch((error) => next(error));
+  try {
+    const noteObj = await Note.findByIdAndUpdate(req.params.id, newNote, { new: true });
+    res.json(noteObj);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // DELETE
-notesRouter.delete("/:id", (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      if (!result) {
-        logger.error("no such note");
-        res.status(404).send("no such note in db");
-      } else {
-        res.status(204).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", async (req, res, next) => {
+  const newNote = await Note.findByIdAndUpdate(req.params.id);
+
+  try {
+    if (!newNote) {
+      logger.error("no such note");
+      res.status(404).send("no such note in db");
+    } else {
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(Error);
+  }
 });
 
 module.exports = notesRouter;
